be2a01f68692fd32ab5d54a962b908ca3fde4c09
irynakrykun <ira_franyk@ukr.net>
2022-10-27 20:46:26 +0300

    work

---
+++ js/01-gallery.js
@@ -0,0 +1,37 @@
+import { galleryItems } from './gallery-items.js';
+
+// Change code below this line
+
+console.log(galleryItems);
+const GalleryAll = document.querySelector('.gallery');
+const GalleryItems = CreateGalleryItems(galleryItems);
+GalleryAll.insertAdjacentHTML('beforeend', GalleryItems)
+
+function CreateGalleryItems(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+      return ` <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div> `;
+    }).join('')
+} 
+
+GalleryAll.addEventListener('click', displayImage);
+function displayImage(e) {
+  e.preventDefault();
+   if (e.target.nodeName !== "IMG") { return };
+   const url = e.target.dataset.source;
+   console.dir(url);
+
+  const instance = basicLightbox.create(`
+  <img
+  src="${url}"
+  width="800"
+  height="600">`).show();
+}
